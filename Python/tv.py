c93b58d84d613e510fad5dde712493bf0b6c03ad
Ryan Araújo <ryanaraujospa@gmail.com>
2024-11-06 17:36:43 -0300

    2.1

--- Python/tv.py
+++ Python/tv.py
@@ -1,23 +1,31 @@
-class televisao:
-    def __init__ (self, pcanal, min, max):
+class Televisão:
+    def __init__(self, pcanal, min, max):
         self.canal = pcanal
         self.cmin = min
         self.cmax = max
 
     def muda_canal_para_baixo(self):
-        self.canal -= 1
-    
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+        else:
+            self.canal = self.cmax
+
     def muda_canal_para_cima(self):
-        self.canal += 1
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
+        else:
+            self.canal = self.cmin
 
-tv1 = televisao(2, 2, 10)
+tv1 = Televisão(9 , 2,  10 )
 print(tv1.canal)
-for x in range(1, 20):
-    tv1.muda_canal_para_cima()
-    print(tv1.canal)
+tv1.muda_canal_para_cima()
+print(tv1.canal)
+tv1.muda_canal_para_cima()
+print(tv1.canal)
 
-tv2 = televisao(10, 2, 10)
+tv2 = Televisão(3 , 2,  10 )
 print(tv2.canal)
-for x in range(1, 20):
-    tv2.muda_canal_para_baixo()
-    print(tv2.canal)
+tv2.muda_canal_para_baixo()
+print(tv2.canal)
+tv2.muda_canal_para_baixo()
+print(tv2.canal)
